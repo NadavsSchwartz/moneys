896e9b39e0209f43d9f2d0e93c84d87e1984a199
nadavschwartz <nadavschwartz58@gmail.com>
2021-03-03 09:20:18 -0800

    redux setup

---
+++ src/components/Auth/Register.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+
+import { connect } from "react-redux";
+import { registerUser } from "../../actions/authActions";
+
+const Register = () => {
+  return <div></div>;
+};
+
+export default Register;
